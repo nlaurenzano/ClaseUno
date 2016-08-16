fcb1a56bb85f6a7b2ce4fd93d728569ef4ec56c1
nlaurenzano <nlaurenzano@gmail.com>
2016-08-16 20:28:16 -0300

    Mando un cambio.

--- index.php
+++ index.php
@@ -12,12 +12,11 @@
 
 	$numero = 10;
 
-	echo "El número es " . $numero;
+//	echo "El número es " . $numero;		//Con "." se puede concatenar: El número es 10
+//	echo "El número es $numero";		//Con comillas dobles se muestra el contenido de las variables: El número es 10
+//	echo 'El número es $numero';		//Con comillas simples se muestra literalmente lo que se escribe: El número es $numero
 
-	echo "El número es $numero";
-
-	echo 'El número es $numero';
-
+	echo "El cambio";
 
 
 	?>
