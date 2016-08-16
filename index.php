d48d850f404ae32c185880bbd89601e8d5f36f88
nlaurenzano <nlaurenzano@gmail.com>
2016-08-16 20:25:10 -0300

    Estoy probando el commit.

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Clase Uno</title>
+</head>
+
+<body>
+	<h1>Hola Mundo!</h1>
+
+	<?php
+	echo "Hola PHP";
+	echo "<h2>Otro hola PHP</h2>";
+
+	$numero = 10;
+
+	echo "El número es " . $numero;
+
+	echo "El número es $numero";
+
+	echo 'El número es $numero';
+
+
+
+	?>
+
+
+</body>
+</html>
